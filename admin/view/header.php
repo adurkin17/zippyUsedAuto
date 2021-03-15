c99353851bd6e5b627aa96d76b0e03325d838bc5
Austin Durkin <ajdurkin@mail.fhsu.edu>
2021-03-15 13:42:09 -0500

    moved index.php

--- admin/view/header.php
+++ admin/view/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zippy's Admin </title>
-    <link rel="stylesheet" hrefs = "sass.main.scss"/>
+    <link rel="stylesheet" hrefs = "../sass/main.css"/>
 </head>
 <body>
     <main class="main">
